19c1c4c1c6096d129e7ad658c34dfbefcb47688b
Gabriel Freire de Barros Vieira <144478152+vieira98@users.noreply.github.com>
2024-11-21 18:44:00 +0000

    commit views

--- task/views.py
+++ task/views.py
@@ -4,3 +4,9 @@ from .models import Tarefa
 def lista_tarefas(request):
     tarefas = Tarefa.objects.all()
     return render(request, 'lista_tarefas.html', {'tarefas': tarefas})
+
+def concluir_tarefa(request, id):
+    tarefa = get_object_or_404(Tarefa, id=id)
+    tarefa.concluida = True
+    tarefa.save()
+    return redirect('lista_tarefas')
