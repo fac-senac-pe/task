56145d02b0e3bc6c43bc81b419250cd60eb7b269
Marcelo Sette <msettejunior@gmail.com>
2024-11-21 16:10:23 -0300

    View update

--- task/views.py
+++ task/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Tarefa
+from django.shortcuts import render, redirect, get_object_or_404
 
 def lista_tarefas(request):
     if request.method == 'POST':
@@ -10,3 +11,12 @@ def lista_tarefas(request):
 
     tarefas = Tarefa.objects.all()
     return render(request, 'lista_tarefas.html', {'tarefas': tarefas})
+
+
+
+
+def concluir_tarefa(request, id):
+    tarefa = get_object_or_404(Tarefa, id=id)
+    tarefa.concluida = True
+    tarefa.save()
+    return redirect('lista_tarefas')
