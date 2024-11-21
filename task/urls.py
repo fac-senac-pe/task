615535477cd07b6a7c9854c9b7711efbde4afd9a
Gabriel Freire de Barros Vieira <144478152+vieira98@users.noreply.github.com>
2024-11-21 18:51:21 +0000

    urls

--- task/urls.py
+++ task/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.lista_tarefas, name='lista_tarefas'),
+    path('concluir/<int:id>/', views.concluir_tarefa, name='concluir_tarefa'),
 ]
+
+
