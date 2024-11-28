fbc7196ebc2fc779e58352ebf6815ea53d76cc41
ronierison.maciel <ronierison.maciel@gmail.com>
2024-11-28 15:56:27 -0300

    :bug: update view

--- task/views.py
+++ task/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Tarefa
 from django.shortcuts import render, redirect, get_object_or_404
 
+
 def lista_tarefas(request):
     if request.method == 'POST':
         titulo = request.POST.get('titulo')
@@ -12,10 +13,7 @@ def lista_tarefas(request):
     tarefas = Tarefa.objects.all()
     return render(request, 'lista_tarefas.html', {'tarefas': tarefas})
 
-def excluir_tarefa(request, id):
-    tarefa = get_object_or_404(Tarefa, id=id)
-    tarefa.delete()
-    return redirect('lista_tarefas')
+
 
 
 def concluir_tarefa(request, id):
