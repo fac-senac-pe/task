c25a8aba14b3a1d575226f70f2f3516f9fa91abc
ronierisonmaciel <ronierison.maciel@gmail.com>
2024-12-05 15:15:02 -0300

    :fire: add user association to tasks and update templates

--- task/models.py
+++ task/models.py
@@ -1,8 +1,11 @@
-from django.db import models
+from django.contrib.auth.models import User, models
+
 
 class Tarefa(models.Model):
     titulo = models.CharField(max_length=100)
     concluida = models.BooleanField(default=False)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
 
     def __str__(self):
         return self.titulo
--- task/urls.py
+++ task/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.lista_tarefas, name='lista_tarefas'),
     path('concluir/<int:id>/', views.concluir_tarefa, name='concluir_tarefa'),
+    path('excluir/<int:id>/', views.excluir_tarefa, name='excluir_tarefa'),
+    path('editar/<int:id>/', views.editar_tarefa, name='editar_tarefa'),
 ]
 
 
