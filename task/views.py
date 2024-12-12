0dc827b358257e5816e4cbe0ff780fbb2a371334
Juliana Moreira <oliveiramjuliana22@gmail.com>
2024-12-11 23:28:50 -0300

    atualização dos formularios e views para o upload de imagens

--- task/views.py
+++ task/views.py
@@ -2,18 +2,28 @@ from django.shortcuts import render, redirect
 from .models import Tarefa
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import Tarefa
+from .forms import TarefaForm
 
 
 @login_required
 def lista_tarefas(request):
     if request.method == 'POST':
-        titulo = request.POST.get('titulo')
-        if titulo:
-            Tarefa.objects.create(titulo=titulo)
+        form = TarefaForm(request.POST, request.FILES)
+        if form.is_valid():
+            tarefa = form.save(commit=False)
+            tarefa.usuario = request.user
+            tarefa.save()
+            messages.success(request, 'Tarefa adicionada com sucesso!')
             return redirect('lista_tarefas')
+        else:
+            messages.error(request, 'Erro ao adicionar a tarefa. Verifique os dados informados.')
+    else:
+        form = TarefaForm()
 
-    tarefas = Tarefa.objects.all()
-    return render(request, 'lista_tarefas.html', {'tarefas': tarefas})
+    tarefas = Tarefa.objects.filter(usuario=request.user)
+    return render(request, 'lista_tarefas.html', {'tarefas': tarefas, 'form': form})
 
 
 def concluir_tarefa(request, id):
@@ -23,17 +33,21 @@ def concluir_tarefa(request, id):
     return redirect('lista_tarefas')
 
   
+@login_required
 def editar_tarefa(request, id):
-    tarefa = get_object_or_404(Tarefa, id=id)
-
+    tarefa = get_object_or_404(Tarefa, id=id, usuario=request.user)
     if request.method == 'POST':
-        novo_titulo = request.POST.get('titulo')
-        if novo_titulo:
-            tarefa.titulo = novo_titulo
-            tarefa.save()
+        form = TarefaForm(request.POST, request.FILES, instance=tarefa)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Tarefa atualizada com sucesso!')
             return redirect('lista_tarefas')
+        else:
+            messages.error(request, 'Erro ao atualizar a tarefa. Verifique os dados informados.')
+    else:
+        form = TarefaForm(instance=tarefa)
 
-    return render(request, 'editar_tarefa.html', {'tarefa': tarefa})
+    return render(request, 'editar_tarefa.html', {'form': form})
 
 
 def excluir_tarefa(request, id):
